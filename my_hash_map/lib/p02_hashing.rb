bf84cdf8bbfcd78f0c19af4309a422f9558afb52
Josh Turgeon <j_turgeon88@yahoo.com>
2017-10-13 15:04:17 -0700

    Finish phase 2

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,30 @@ end
 
 class Array
   def hash
+    hash_machine = {
+      1 => 12,
+      2 => 20,
+      3 => 25,
+      4 => 6,
+
+    }
+
+    counter = 10
+    ("a".."z").each do |char|
+      counter += 1
+      hash_machine[char] = counter
+    end
+    id = ""
+    self.each do |el|
+      id += hash_machine[el].to_s
+    end
+    id.to_i
   end
 end
 
 class String
   def hash
+    self.downcase.chars.hash
   end
 end
 
@@ -16,6 +35,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    id = 0
+    self.each do |k, v|
+      id += v.hash
+    end
+    id
   end
 end
